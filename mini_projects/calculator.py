54d144669f4b3e7010d9110f49e2bb7573de0e58
Kunalkhairnar18 <kunalkhairnar53@gmail.com>
2025-12-26 00:56:44 +0530

    Basic calculator program

---
+++ mini_projects/calculator.py
@@ -0,0 +1,17 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+
+add=a+b
+print("addition is",add)
+
+sub=a-b
+print("subtraction is",sub)
+
+mul=a*b
+print("multiplication is",mul)
+
+div=a/b
+print("division is",div)
+
+floor_division=a//b
+print("floor division is",floor_division)
